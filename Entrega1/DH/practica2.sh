61ba362509ed2f548e3fd25eaf5fc62a078ff156
kevin santana <kevin_santana120@hotmail.com>
2023-06-06 19:24:17 -0500

    entrega 1

---
+++ Entrega1/DH/practica2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir Peliculas Peliculas/Infantiles Peliculas/Accion Peliculas/Terror Peliculas/Comedia Peliculas/Romanticas
+touch Peliculas/Infantiles/Intenzamente.txt Peliculas/Infantiles/"Eljuego del miedo.xsl" Peliculas/Infantiles/"Rapido y furioso.pdf" Peliculas/Infantiles/Titanic.jpg
+touch Peliculas/Accion/"Smooth criminal.txt" Peliculas/Accion/"Diario de una pasion.js" Peliculas/Accion/"El hexorcista.xsl" Peliculas/Accion/Cars.jpg
+touch Peliculas/Terror/"La dama y el bagavundo.png" Peliculas/Terror/"Yo antes de ti.xls" Peliculas/Terror/"Que paso ayer.pdf" Peliculas/Terror/"We will rock you.js"
+touch Peliculas/Comedia/Madagaskar.js Peliculas/Comedia/"Orgullo y prejuicio.txt" Peliculas/Comedia/IT.xls Peliculas/Comedia/"Mision imposible.png"
+touch Peliculas/Romanticas/Pocahontas.jpg Peliculas/Romanticas/Annabelle.js Peliculas/Romanticas/"Los vengadores.js" Peliculas/Romanticas/"La mascara.js"
+
+
